57142cf5e817260193bb19299e31ed6e80067856
tech-nisshi <bochibochiicoca012@gmail.com>
2019-07-23 20:17:26 +0900

    fixed_bug new_group

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -19,7 +19,7 @@ $(function(){
   }
 
   function appendUserTomembersList(name, user_id){
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user_id }'>
                   <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
                   <p class='chat-group-user__name'>${ name }</p>
                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
